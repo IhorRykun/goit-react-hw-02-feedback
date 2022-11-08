7fd8744a9a1e387952dd999ecf317db4bca96d08
Ihor <ihorrykun@gmail.com>
2022-11-08 13:28:55 +0200

    update

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,1 +1,20 @@
-import React, { Component } from 'react';
+import css from '../feedback/feedback.module.css';
+
+export const Feedback = () => {
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>Please leave feedback</h2>
+      <ul className={css.list}>
+        <li>
+          <button className={css.button}>Good</button>
+        </li>
+        <li>
+          <button className={css.button}>Neutral</button>
+        </li>
+        <li>
+          <button className={css.button}>Bad</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
