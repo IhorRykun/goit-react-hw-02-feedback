be70d354d17e131122571c453d4a4685771331b9
Ihor <ihorrykun@gmail.com>
2022-11-08 13:35:22 +0200

    update

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -15,6 +15,17 @@ export const Feedback = () => {
           <button className={css.button}>Bad</button>
         </li>
       </ul>
+      <ul className={css.list__text}>
+        <li className={css.list__text__item}>
+          <p>Good:</p>
+        </li>
+        <li className={css.list__text__item}>
+          <p>Neutral:</p>
+        </li>
+        <li className={css.list__text__item}>
+          <p>Bad:</p>
+        </li>
+      </ul>
     </div>
   );
 };
