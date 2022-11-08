c3349e21edc2c019779b6efb28659538f307bddc
Ihor <ihorrykun@gmail.com>
2022-11-08 21:18:25 +0200

    Update feedback.jsx

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,31 +1,40 @@
 import css from '../feedback/feedback.module.css';
+import React, { Component } from 'react';
 
-export const Feedback = () => {
-  return (
-    <div className={css.container}>
-      <h2 className={css.title}>Please leave feedback</h2>
-      <ul className={css.list}>
-        <li>
-          <button className={css.button}>Good</button>
-        </li>
-        <li>
-          <button className={css.button}>Neutral</button>
-        </li>
-        <li>
-          <button className={css.button}>Bad</button>
-        </li>
-      </ul>
-      <ul className={css.list__text}>
-        <li className={css.list__text__item}>
-          <p>Good:</p>
-        </li>
-        <li className={css.list__text__item}>
-          <p>Neutral:</p>
-        </li>
-        <li className={css.list__text__item}>
-          <p>Bad:</p>
-        </li>
-      </ul>
-    </div>
-  );
-};
+export class Feedback extends Component {
+  handleIncrement =() => {
+    console.log('click mouse');
+  }
+
+  render() {
+    return (
+      <div className={css.container}>
+        <h2 className={css.title}>Please leave feedback</h2>
+        <ul className={css.list}>
+          <li>
+            <button onClick={this.handleIncrement} className={css.button}>
+              Good
+            </button>
+          </li>
+          <li>
+            <button className={css.button}>Neutral</button>
+          </li>
+          <li>
+            <button className={css.button}>Bad</button>
+          </li>
+        </ul>
+        <ul className={css.list__text}>
+          <li className={css.list__text__item}>
+            <p>Good:</p>
+          </li>
+          <li className={css.list__text__item}>
+            <p>Neutral:</p>
+          </li>
+          <li className={css.list__text__item}>
+            <p>Bad:</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
